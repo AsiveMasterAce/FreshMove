fc9cfa6d041259051f1e5a9124f889dd45f0e163
s220029881 <s220029881@M104-79>
2023-04-12 15:12:15 +0200

    Add project files.

---
+++ FreshMove/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreshMove.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
