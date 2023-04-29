12785e2d700ef0bf54bd225066cf491bc27c14cd
Asive Nzwana <s220029881@mandela.ac.za>
2023-04-29 11:18:39 +0200

    Adding the userRoles

---
+++ FreshMove/Data/ContextSeed.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SalesOrders.Models.users;
+using FreshMove.Constants;
+namespace FreshMove.Data
+{
+    public class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(RoleConstants.Admin));
+
+        }
+    }
+}
