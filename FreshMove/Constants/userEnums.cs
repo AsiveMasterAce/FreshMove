60c5cb5f05270b5e410fa9654781e7591effcc00
Asive Nzwana <s220029881@mandela.ac.za>
2023-05-04 14:35:44 +0200

    Adding Migration  and Users

---
+++ FreshMove/Constants/userEnums.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FreshMove.Constants
+{
+  public enum Gender
+  {
+    [Display(Name ="Female")]
+     Female,
+    [Display(Name = "Male")]
+     Male,
+    [Display(Name = "Other")]
+     Other
+  }
+  public enum Race
+  {
+    [Display(Name = "African")]
+     African,
+    [Display(Name = "White")]
+     White,
+    [Display(Name = "Colored")]
+     Colored,
+    [Display(Name = "Indian")]
+     Indian,
+    [Display(Name = "Other")]
+     Other
+  }
+
+}
