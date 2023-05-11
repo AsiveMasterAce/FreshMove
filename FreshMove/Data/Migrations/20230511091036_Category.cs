c49fbd3669267c170b6459a46a45136fb0af4959
Asive Nzwana <s220029881@mandela.ac.za>
2023-05-11 15:58:07 +0200

    Category changes

---
+++ FreshMove/Data/Migrations/20230511091036_Category.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreshMove.Migrations
+{
+    public partial class Category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Archived = table.Column<bool>(type: "bit", nullable: false),
+                    categoryImage = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ FreshMove/Models/categories/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace FreshMove.Models.categories
+{
+    public class CategoryViewModel
+    {
+        [Display(Name = "Name")]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        [StringLength(100)]
+        public string Description { get; set; }
+        [NotMapped]
+        [Display(Name = "Category Image")]
+        public IFormFile categoryImage { get; set; }
+
+    }
+}
