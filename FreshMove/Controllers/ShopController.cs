55a75f5babbaa86dc13edffefa18ea959fd243b6
Asive Nzwana <s220029881@mandela.ac.za>
2023-05-16 11:17:19 +0200

    UI Shop Now Added and made changes

---
+++ FreshMove/Controllers/ShopController.cs
@@ -0,0 +1,35 @@
+﻿
+using FreshMove.Data;
+using FreshMove.Models.ViewModels.Shop;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreshMove.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ShopController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+
+            var products = _context.Products.Where(p => p.Archive == false).ToList();
+
+            var viewModel = new ShopViewModel
+            {
+                products = products
+            };
+            return View(viewModel);
+        }
+    }
+}
